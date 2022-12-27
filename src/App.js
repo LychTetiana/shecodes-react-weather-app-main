cac13281b39c85af17373ad83b94562eb7cde609
LychTetiana <tetiana_tomashchuk@ukr.net>
2022-12-27 13:08:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default function App() {
           </a>{" "}
           and{" "}
           <a
-            href=""
+            href="http://localhost:3000/"
             target="_blank"
             rel="noopener noreferrer"
           >
